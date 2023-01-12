4dc2529323c2c9d857fab23e920c6fc5db0e550a
Edwin Pers <pers.edwin@gmail.com>
2023-01-12 15:58:06 +0000

    make db info specification more sane

--- wwwroot/config.php
+++ wwwroot/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define("MYSQL_HOST", db);
-define("MYSQL_USER", getenv('DB_USERNAME'));
-define("MYSQL_PASSWORD", getenv('DB_PASSWORD'));
-define("MYSQL_DATABASE", getenv('DB_DATABASE'));
+define("MYSQL_HOST", getenv('MYSQL_HOST'));
+define("MYSQL_USER", getenv('MYSQL_USER'));
+define("MYSQL_PASSWORD", getenv('MYSQL_PASSWORD'));
+define("MYSQL_DATABASE", getenv('MYSQL_DATABASE'));
 
 ?>
